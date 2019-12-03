394f600bcfc043f02580a8d26254699b5f4a49f9
Daleko <54468177+Daleko@users.noreply.github.com>
2019-12-03 14:32:08 +0100

    Add files via upload

--- src/internet_sales/Internet_Sales.java
+++ src/internet_sales/Internet_Sales.java
@@ -1,1 +1,55 @@
-1
+/**
+ *
+ * @author Aleksandra George
+ */
+package internet_sales;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Internet_Sales extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/InternetSales.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/view/style.css");
+        Image icon = new Image(getClass().getResourceAsStream("/view/icon.png"));
+        stage.getIcons().add(icon);
+        stage.initStyle(StageStyle.TRANSPARENT);
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
